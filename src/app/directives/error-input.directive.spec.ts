947147a937b618cf257558c204a821f23323e5c4
Serhii <chuhaystyr@gmail.com>
2020-01-08 22:47:33 +0200

    del test

--- src/app/directives/error-input.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ErrorInputDirective } from './error-input.directive';
-
-describe('ErrorInputDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ErrorInputDirective();
-    expect(directive).toBeTruthy();
-  });
-});
