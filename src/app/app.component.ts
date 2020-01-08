d4f306b84ac9d2dc02b48cfbd5bc239ebc507c13
Serhii <chuhaystyr@gmail.com>
2020-01-08 22:45:32 +0200

    task1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,16 @@
-import { Component } from '@angular/core';
-import {NgForm} from '@angular/forms';
-
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
   formSubmitted: boolean;
-  name: string = ""; // Если записать undefined, то поля изначально не будет в значении формы.
-  country: string; // Здесь связывание у нас одностороннее, поэтому начальное значение можем не задавать.
-  
-  countryModelChange(value: string) {
-    this.country = value;
-  }
-  
-  submit(event: Event) {
-    event.preventDefault();
-    this.formSubmitted = true;
+  num: string = "";
+
+  isNumber(num) {
+    return isNaN(+num);
   }
 }
